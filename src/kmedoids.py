967fddc7a12656502a554a8b141962066710e32f
Calvin Wylie <calvin.wylie@gmail.com>
2015-04-14 18:23:04 -0400

    multiswaps.py, kmedoids.py

---
+++ src/kmedoids.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.metrics.pairwise import pairwise_distances
+
+def cluster(distances, k=3):
+
+    m = distances.shape[0] # number of points
+
+    # Pick k random medoids.
+    curr_medoids = np.array([-1]*k)
+    while not len(np.unique(curr_medoids)) == k:
+        curr_medoids = np.array([random.randint(0, m - 1) for _ in range(k)])
+    old_medoids = np.array([-1]*k) # Doesn't matter what we initialize these to.
+    new_medoids = np.array([-1]*k)
+   
+    # Until the medoids stop updating, do the following:
+    while not ((old_medoids == curr_medoids).all()):
+        # Assign each point to cluster with closest medoid.
+        clusters = assign_points_to_clusters(curr_medoids, distances)
+
+        # Update cluster medoids to be lowest cost point. 
+        for curr_medoid in curr_medoids:
+            cluster = np.where(clusters == curr_medoid)[0]
+            new_medoids[curr_medoids == curr_medoid] = compute_new_medoid(cluster, distances)
+
+        old_medoids[:] = curr_medoids[:]
+        curr_medoids[:] = new_medoids[:]
+
+    return clusters, curr_medoids
+
+def assign_points_to_clusters(medoids, distances):
+    distances_to_medoids = distances[:,medoids]
+    clusters = medoids[np.argmin(distances_to_medoids, axis=1)]
+    clusters[medoids] = medoids
+    return clusters
+
+def compute_new_medoid(cluster, distances):
+    mask = np.ones(distances.shape)
+    mask[np.ix_(cluster,cluster)] = 0.
+    cluster_distances = np.ma.masked_array(data=distances, mask=mask, fill_value=10e9)
+    costs = cluster_distances.sum(axis=1)
+    return costs.argmin(axis=0, fill_value=10e9)
+
+if __name__ == '__main__':
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5,
+                                random_state=999)
+    distances = pairwise_distances(X)
+
+    clusters, curr_medoids = cluster(distances, k=3)
+    print(curr_medoids)
+
+
+
+
+
---
+++ src/multiswaps.py
@@ -0,0 +1,64 @@
+import random
+import itertools
+
+import numpy as np
+
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.metrics.pairwise import pairwise_distances
+
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+num_clusters = 3
+p = 2
+epsilon = 5
+
+def find_closest_median(i, medoids, distances):
+    return medoids[np.argmin(distances[i, medoids])]
+
+def objective(medoids, distances):
+    n = distances.shape[0]
+    s = 0
+    for i in range(0, n):
+        s += distances[i, find_closest_median(i, medoids, distances)]
+    return s
+
+def swap(medoids, A, B):
+    tmp = medoids
+    for i, j in zip(A, B):
+        tmp = swap_single(tmp, i, j)
+    return tmp
+
+def swap_single(medoids, a, b):
+    return [b if i == a else i for i in medoids]
+
+def search_for_swap(medoids, distances):
+    prev_objective = objective(medoids, distances)
+    improvement = False
+    for A in itertools.combinations(medoids, p):
+        for B in itertools.combinations(range(n), p):
+            new_medoids = swap(medoids, A, B)
+            if objective(new_medoids, distances) < prev_objective - epsilon:
+                improvement = True
+                print("Improvement: %f" % (prev_objective - objective(new_medoids, distances)))
+                return new_medoids, improvement
+    return medoids, improvement
+
+if __name__ == '__main__':
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5,
+                                random_state=999)
+    distances = pairwise_distances(X)
+    n = distances.shape[0]
+    
+    #Choose random medoids
+    # medoids = random.sample(range(n), num_clusters)
+
+    medoids = [0, 1, 2]
+    print(medoids, objective(medoids, distances))
+
+    improvement = True
+    while improvement:
+        medoids, improvement = search_for_swap(medoids, distances)
+
+    print(medoids, objective(medoids, distances))
