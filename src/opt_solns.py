7442e3df117ef4a3b185d7bda6bf065704d608d2
Alice Paul <ajp336@cornell.edu>
2015-04-20 14:39:49 -0400

    Added all presentation files and associated images

--- src/opt_solns.py
+++ src/opt_solns.py
@@ -5,7 +5,7 @@ from LoadData import *
 from gurobipy import *
 from sklearn.metrics.pairwise import pairwise_distances
 
-def kmedian_opt(distances, IP, k):
+def kmedian_opt(distances, IP, k1, k2):
     model = Model("k-median")
     n = np.shape(distances)[0]
     y,x = {}, {}
@@ -31,25 +31,25 @@ def kmedian_opt(distances, IP, k):
     for j in range(n):
         for i in range(n):
             model.addConstr(x[i,j], "<", y[j], name="Strong[%s,%s]"%(i,j))
-            
-    coef = [1 for j in range(n)]
-    var = [y[j] for j in range(n)]
 
     model.setParam( 'OutputFlag', False )
     model.__data = x,y
     outputs = []
     model.update()
-    
-    k_constr = model.addConstr(LinExpr(coef,var), "=", rhs=k, name="k_median")
-    model.update()
-    model.optimize()
 
-    if model.status == GRB.status.OPTIMAL:
-        print model.objVal
-        outputs.append(model.objVal)
-    else:
-        print "not optimal"
-        output.append(0)
+    for k in range(k1, k2):
+        coef = [1 for j in range(n)]
+        var = [y[j] for j in range(n)]
+        if k > k1:
+            model.remove(k_constr)
+        k_constr = model.addConstr(LinExpr(coef,var), "<", rhs=k)
+        model.update()
+        model.optimize()
+
+        if model.status == GRB.status.OPTIMAL:
+            outputs.append(model.objVal)
+        else:
+            outputs.append(0)
             
     return outputs
 
@@ -57,9 +57,9 @@ def write_opt_bounds(distances, filepath, IP=1):
     f = open(filepath, 'w+')
     n = np.shape(distances)[0]
 
-    bounds = kmedian_opt(distances, IP)
+    bounds = kmedian_opt(distances, IP, 1, n+1)
     for k in range(1,n+1):
-        f.write(str(k)+" "+str(bound[k])+"\n")
+        f.write(str(k)+" "+str(bounds[k-1])+"\n")
 
 def write_opt_pmedians(path_files, file_bounds):
     g = open(file_bounds, 'w+')
@@ -67,7 +67,7 @@ def write_opt_pmedians(path_files, file_bounds):
     for f in os.listdir(path_files):
         print f
         distances, n, k = LoadpMedian(path_files+"\\"+f)
-        bound = kmedian_opt(distances, 1, k)
+        bound = kmedian_opt(distances, 1, k, k+1)
         g.write(f+" "+str(bound)+"\n")
 
 def write_opt_data(path_files, file_bounds):
@@ -77,8 +77,16 @@ def write_opt_data(path_files, file_bounds):
         print f
         X, y, n, k = LoadData(path_files+"\\"+f)
         distances = pairwise_distances(X)
-        bound = kmedian_opt(distances, 1, k)
+        bound = kmedian_opt(distances, 1, k, k+1)
         g.write(f+" "+str(bound)+"\n")
+
+def write_opt_hier_data(path_files, path_bounds):
+    for f in os.listdir(path_files):
+        print f
+        X, y, n, k = LoadData(path_files+"\\"+f)
+        distances = pairwise_distances(X)
+        write_opt_bounds(distances, path_bounds+"\\"+f)
+    
         
     
     
