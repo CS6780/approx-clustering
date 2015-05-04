68ee8a2f829697a9272b9839681211e863a4a128
Alice Paul <ajp336@cornell.edu>
2015-05-04 10:24:56 -0400

    multiswap sped up

--- src/multiswaps.py
+++ src/multiswaps.py
@@ -1,16 +1,35 @@
 import random
 import itertools
 import numpy as np
+from scipy.special import binom
+
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.metrics.pairwise import pairwise_distances
 
 def find_closest_median(i, medoids, distances):
-    return medoids[np.argmin(distances[i, medoids])]
+    closest = None
+    closest_dist = float("inf")
+    
+    for j in medoids:
+        if distances[i][j] < closest_dist:
+            closest_dist = distances[i][j]
+            closest = j
+        
+    return closest
 
-def objective(medoids, distances):
+def objective(medoids, distances, FDict):
     n = distances.shape[0]
-    s = 0
-    for i in range(0, n):
-        s += distances[i, find_closest_median(i, medoids, distances)]
-    return s
+
+    if medoids in FDict:
+        return FDict[medoids]
+        
+    Sum = 0
+    for x in range(n):
+        if x not in medoids:
+            distx = [distances[x][s] for s in medoids]
+            Sum+=min(distx)
+    FDict[medoids] = Sum
+    return Sum
 
 def swap(medoids, A, B):
     tmp = medoids
@@ -21,31 +40,68 @@ def swap(medoids, A, B):
 def swap_single(medoids, a, b):
     return [b if i == a else i for i in medoids]
 
-def search_for_swap(medoids, distances, p=2, epsilon=0):
+def search_for_swap(medoids, obj, distances, FDict, p=2, delta=0):
     n = distances.shape[0]
-    prev_objective = objective(medoids, distances)
+    prev_objective = obj
+    medoids_list = list(medoids)
+    medoids_c_list = [i for i in range(n) if i not in medoids]
+    medoids_c = frozenset(medoids_c_list)
+    
     improvement = False
-    for A in itertools.combinations(medoids, p):
-        for B in itertools.combinations(range(n), p):
-            new_medoids = swap(medoids, A, B)
-            if objective(new_medoids, distances) < prev_objective - epsilon:
-                improvement = True
-                print("Improvement: %f" % (prev_objective - objective(new_medoids, distances)))
-                return new_medoids, improvement
-    return medoids, improvement
+    for q in range(1,p+1):
+        if q == 1:
+            for A in itertools.combinations(medoids, q):
+                for B in itertools.combinations(medoids_c, q):
+                    new_medoids = medoids.difference(A).union(B)
+                    new_objective = objective(new_medoids, distances, FDict)
+                    if new_objective < (1.0-delta)*prev_objective:
+                        improvement = True
+                        #print("Improvement: %f" % (prev_objective - new_objective))
+                        return new_medoids, new_objective, improvement
+        else:
+            iters = 0
+            while iters < n*q:
+                iters += 1
+                A = np.random.choice(medoids_list,size=q,replace=False)
+                B = np.random.choice(medoids_c_list,size=q,replace=False)
+                new_medoids = medoids.difference(A).union(B)
+                new_objective = objective(new_medoids, distances, FDict)
+                if new_objective < (1.0-delta)*prev_objective:
+                    improvement = True
+                    #print("Improvement: %f" % (prev_objective - new_objective))
+                    return new_medoids, new_objective, improvement
+                
+    return medoids, prev_objective, improvement
 
 def predict(medoids, distances):
     n = distances.shape[0]
     return [ find_closest_median(i, medoids, distances) for i in range(n) ]
 
-def cluster(distances, k, p, epsilon=0):
+def cluster(distances, k, p, warm_start = None, epsilon=0):
     n = distances.shape[0]
-    medoids = random.sample(range(n), k)
+    delta = epsilon/float(binom(n-k,p)*binom(k,p))
+    #print delta
+    if warm_start is None:
+        medoids = frozenset(np.random.choice([i for i in range(n)],size=k,replace=False))
+    else:
+        medoids = frozenset(warm_start)
+        
+    FDict = {}
+    obj = objective(medoids, distances, FDict)
+    
     improvement = True
     while improvement:
-        medoids, improvement = search_for_swap(medoids, distances, p, epsilon=epsilon)
+        medoids, obj, improvement = search_for_swap(medoids, obj, distances, FDict, \
+                                                    p, delta)
 
-    return predict(medoids, distances), medoids
+    return predict(medoids, distances), list(medoids)
+
+if __name__ == '__main__':
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, labels_true = make_blobs(n_samples=900, centers=centers, cluster_std=0.5,
+                            random_state=999)
+    distances = pairwise_distances(X)
+    print cluster(distances,3,2,[0,1,2],0.1)
 
 
 
